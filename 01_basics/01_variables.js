47ee5e79d8a1727b936c5a5612efd1c47b2e4ba1
AzfarMd <23je0593@iitism.ac.in>
2026-05-11 07:50:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const accountID=1234
+let accountEmail="azfar212@gmail.com"
+let accountstate
+console.table([accountID,accountEmail,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) we are using node.js not browser
+//code shpiuld be readable
+let num
+let name
+let boolien
+//string=> use ""
+/*
+null=>standalone value
+undefined=>value not assigned
+symbol=>unique
+type of null=>object and of undefined=>undefined
+*/
+console.log(typeof null);
